499786eedfb32f2599726d45a397892b1a8f2b23
Xannydayo <yogaviveka1234@gmail.com>
2024-06-09 20:06:09 +0700

    first commit

---
+++ script.js
@@ -0,0 +1,5 @@
+alert('hallo selamat datang di web anime');
+
+document.getElementById('main-text').innerHTML = 'Selamat datang di Web Anime!';
+
+document.getElementById('footer-text').innerHTML = 'Hak Cipta &copy; 2024 Web Anime. Semua hak dilindungi.';
